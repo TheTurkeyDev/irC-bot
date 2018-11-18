255ad13c35486f61d96bdf60653749a3f3b4a1d2
Matt <mgcohen215@gmail.com>
2018-11-18 14:25:34 -0500

    Added command to get basic system info

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <connection.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/utsname.h>
 
 void onIRCMessage(char* msg, char* from) {
 	char* command = strtok (msg," ");
@@ -10,6 +11,35 @@ void onIRCMessage(char* msg, char* from) {
 		strcat(msg, from);
 		strcat(msg, " !\0");
 	}
+
+	if(strcmp(command, "!info")) {
+		char msg[512];
+
+		struct utsname unameInfo;
+		uname(&unameInfo);
+
+		strcat(msg, "System: ");
+		strcat(msg, unameInfo.sysname);
+		strcat(msg, "\n");
+
+		strcat(msg, "Node: ");
+		strcat(msg, unameInfo.nodename);
+		strcat(msg, "\n");
+
+		strcat(msg, "Release: ");
+		strcat(msg, unameInfo.release);
+		strcat(msg, "\n");
+
+		strcat(msg, "Version: ");
+		strcat(msg, unameInfo.version);
+		strcat(msg, "\n");
+
+		strcat(msg, "Machine: ");
+		strcat(msg, unameInfo.machine);
+		strcat(msg, "\n");
+
+	}
+
 	printf("Recieved a message from: %s\"%s\"\n", from, msg);
 }
 
